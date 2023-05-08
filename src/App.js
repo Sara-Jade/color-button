c3f427774b6ad53daae786bd28d761ba611d3ead
SuperJuicer <Sara.Jade@alaskaair.com>
2023-05-08 15:09:36 -0700

    toggleButtonColor written. Now tests pass.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [buttonColor, toggleButtonColor] = useState('red');
+  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
   return (
-      <button style={{ backgroundColor: 'red' }}>Change to blue</button>
+    <button 
+      style={{ backgroundColor: buttonColor }}
+      onClick={() => toggleButtonColor(newButtonColor)}
+    >
+      Change to {newButtonColor}
+    </button>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { logRoles, render, screen } from '@testing-library/react';
+import { fireEvent, logRoles, render, screen } from '@testing-library/react';
 import App from './App';
 
 // // Better to getByRole than by text
@@ -21,13 +21,38 @@ test('that the button is initially red', () => {
 });
 
 test('that the button initially has the text Change to blue', () => {
+  render(<App />);
+  const button = screen.getByRole('button', { name: 'Change to blue' });
 
+  expect(button).toHaveTextContent('Change to blue');
 });
 
 test('that the button turns blue when clicked', () => {
+  // Arrange
+  render(<App />);
+  const button = screen.getByRole('button', { name: 'Change to blue' });
 
+  // Initial assert
+  expect(button).toHaveStyle({ backgroundColor: 'red' });
+
+  // Act
+  fireEvent.click(button);
+
+  // Final assert
+  expect(button).toHaveStyle({ backgroundColor: 'blue' });
 });
 
 test('that the button text changes to Change to red when clicked', () => {
+  // Arrange
+  render(<App />);
+  const button = screen.getByRole('button', { name: 'Change to blue' });
 
+  // Initial assert
+  expect(button).toHaveTextContent('Change to blue');
+
+  // Act
+  fireEvent.click(button);
+
+  // Final assert
+  expect(button).toHaveTextContent('Change to red');
 });
